a40f026701eb7a992fe520c1e918cd2fd6710469
Jesus 93 <oajesart@hotmail.com>
2019-12-11 07:40:21 -0600

    Acceo a base de datos generica

---
+++ DAL_AXITY/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_AXITY
+{
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+        internal AxityEntities context;
+        internal DbSet<TEntity> dbSet;
+
+        public GenericRepository(AxityEntities context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProporties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach(var includeProperty in includeProporties.Split(
+                new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+
+        }
+    }
+}
---
+++ DAL_AXITY/UnitOfWork.cs
@@ -0,0 +1,82 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_AXITY
+{
+    public class UnitOfWork : IDisposable
+    {
+        private AxityEntities _context = new AxityEntities();
+        private GenericRepository<Products> _products;
+        private GenericRepository<Cat_Products> _catProducts;
+        private GenericRepository<Users> _users;
+
+        public GenericRepository<Products> ProductsRepository
+        {
+            get
+            {
+                if(this._products == null)
+                {
+                    this._products = new GenericRepository<Products>(_context);
+                }
+
+                return _products;
+            }
+        }
+
+        public GenericRepository<Cat_Products> CatProductsRepository
+        {
+            get
+            {
+                if(this._catProducts == null)
+                {
+                    this._catProducts = new GenericRepository<Cat_Products>(_context);
+                }
+
+                return _catProducts;
+            }
+        }
+
+        public GenericRepository<Users> UsersRepository
+        {
+            get
+            {
+                if(this._users == null)
+                {
+                    this._users = new GenericRepository<Users>(_context);
+
+                }
+
+                return this._users;
+            }
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
